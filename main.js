bbd1f723d67ea8599421b3ee24347a62ee8be78b
Tanu <tanupandey291099@gmail.com>
2022-11-09 21:49:12 +0530

    added reference variable to store html element

---
+++ main.js
@@ -0,0 +1,8 @@
+let id = (id) => document.getElementById('id');
+let classes = (classes) => document.getElementsByClassName('classes');
+let username = id('username');
+let email = id('email');
+let password = id('password');
+let form = id('form');
+let errorMs = classes('error');
+
